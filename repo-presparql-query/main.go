735a869107c8713b7d1ddb97d7c5f7d1f781cefb
Kevin Haller <contact@kevinhaller.dev>
2022-09-04 19:20:07 +0200

    Introduces a new logging methodology (using the default Go logging library) for the main file of the sparql query application. It has to be applied to the rest of the code files.

--- repo-presparql-query/main.go
+++ repo-presparql-query/main.go
@@ -1,44 +1,71 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "strings"
+	"fmt"
+	"log"
+	"os"
+	"strings"
 )
 
 const LogPrefix string = "[SPARQL PRELOAD]"
 
-func printUsage() {
-    appName := "graphdb-presparql-query"
-    if len(os.Args) > 0 {
-        appName = os.Args[0]
-    }
-    fmt.Printf("%s <repos-init-directory>\n", appName)
+var (
+	InfoLogger    *log.Logger
+	WarningLogger *log.Logger
+	ErrorLogger   *log.Logger
+)
+
+func init() {
+	InfoLogger = log.New(os.Stdout, LogPrefix+"[INFO ] ", log.Ldate|log.Ltime)
+	WarningLogger = log.New(os.Stdout, LogPrefix+"[WARN ] ", log.Ldate|log.Ltime)
+	ErrorLogger = log.New(os.Stdout, LogPrefix+"[ERROR] ", log.Ldate|log.Ltime)
+}
+
+// printUsage prints a usage message with the specified error message. If the
+// given error message is an empty string, then the error line will be omitted.
+func printUsage(errorMsg string) {
+	errorLine := ""
+	if errorMsg != "" {
+		errorLine = fmt.Sprintf("error: %s\n", errorMsg)
+	}
+	appName := "graphdb-presparql-query"
+	if len(os.Args) > 0 {
+		appName = os.Args[0]
+	}
+	fmt.Printf("%s\nusage:\n\t%s <repos-init-directory>\n",
+		errorLine, appName)
 }
 
 func main() {
-    if len(os.Args) == 2 {
-        fmt.Printf("%s Starting to check whether SPARQL queries need to be executed.\n", LogPrefix)
-        directoryPath := os.Args[1]
-        repoFolders, err := ScanForRepositories(directoryPath)
-        if err == nil {
-            fmt.Printf("%s Detected following repository folders: [%s].\n", LogPrefix,
-                strings.Join(repoFolders, ","))
-            defer func() {
-                if err := recover(); err != nil {
-                    fmt.Printf("%s %v \n", LogPrefix, err)
-                    os.Exit(1)
-                }
-            }()
-            for _, repoFolder := range repoFolders {
-                PreQuery(repoFolder)
-            }
-        } else {
-            fmt.Printf("%s Could not check the subfolders of directory '%s'. %s\n", LogPrefix, directoryPath,
-                err.Error())
-        }
-    } else {
-        printUsage()
-        os.Exit(1)
-    }
+	n := len(os.Args)
+	if n != 2 {
+		if n < 2 {
+			printUsage("not enough arguments specified")
+		} else {
+			printUsage("too many arguments specified")
+		}
+		os.Exit(1)
+	}
+	repositoryInitDirectoryPath := os.Args[1]
+	if !dExists(repositoryInitDirectoryPath) {
+		os.Exit(0)
+	}
+	InfoLogger.Printf("starting to check whether SPARQL queries need to be executed")
+	repoFolders, err := ScanForRepositories(repositoryInitDirectoryPath)
+	if err != nil {
+		ErrorLogger.Printf("couldn't check the subfolders of directory '%s': %s",
+			repositoryInitDirectoryPath, err.Error())
+		os.Exit(1)
+	}
+	InfoLogger.Printf("detected following repository folders: [%s]",
+		strings.Join(repoFolders, ","))
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Printf("%s %v \n", LogPrefix, err)
+			os.Exit(1)
+		}
+	}()
+	for _, repoFolder := range repoFolders {
+		PreQuery(repoFolder)
+	}
 }
