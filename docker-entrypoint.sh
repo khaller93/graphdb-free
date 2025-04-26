526b273568de3d6388bf3dc356a9cf950c1c66cf
Kevin Haller <contact@kevinhaller.dev>
2025-04-26 23:30:08 +0200

    Fixes the grep regex command to properly find the line the passwd file.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,7 @@ mkdir -p /repository.init
 mkdir -p /tmp/toLoad.tmp
 
 if [ "$USER" != "root" -a "$USER" != "0" ]; then
-  USER_PWD=$(cat /etc/passwd | grep -e "(^$USER:|:$USER:)" || true)
+  USER_PWD=$(cat /etc/passwd | grep -e "^$USER:" -e ":$USER:" || true)
   if [ -z "$USER_PWD" ]; then
     if [ $(expr "$USER" : "^[0-9]*$") -eq 0 ]; then
       useradd --no-create-home $USER
